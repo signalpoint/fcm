2652b747ddaa412c62aaf403541c08beef669394
scotthooker <scott.hooker@commerceguys.com>
2014-02-04 12:09:20 +0000

    Updates to include the service calls

--- push_notifications.js
+++ push_notifications.js
@@ -1,25 +1,23 @@
-var GCM_SENDER_ID = 0
+var GCM_SENDER_ID = 111
 /**
  * Implements hook_deviceready().
  */
+
 function push_notifications_deviceready() {
+  Drupal.user.uid = 1;
   try {
     // When the device is connected, if the user is anonymous we don't want to register a token
     if (Drupal.user.uid == 0) {
-      return;
     } else {
-
+      push_notifications_register();
     }
   }
   catch (error) {
     console.log('push_notifications_deviceready - ' + error);
   }
 }
-function push_notifications_tokenHandler(result) {
-  var push_token = localStorage.getItem('push_token');
-  if (push_token === null || push_token != result) {
-    console.log('jere');
-  }
+function push_notifications_tokenHandler(token) {
+  push_notifications_register_device_token(token);
 }
 
 function push_notifications_register() {
@@ -39,15 +37,39 @@ function push_notifications_services_postprocess(options, result) {
     // When the user login service resource is successful store a token
     if (options.service == 'user') {
       if (options.resource == 'login') {
-        // store
+        push_notifications_register();
       }
       else if (options.resource == 'logout') {
-        // delete
+
       }
     }
   }
   catch (error) {
     console('push_notifications_services_postprocess - ' + error);
+  }
+}
+
+function push_notifications_register_device_token(token) {
+  var push_token = localStorage.getItem('push_notifications_token');
+  if (push_token === null || push_token != token) {
+    var data = {
+      'token': token,
+      'type': device.platform,
+    };
+    push_notificiations_create(data, {
+      success: function(result) {
+      }
+    });
+  }
+}
+
+function push_notifications_delete_device_token() {
+  var push_token = localStorage.getItem('push_notifications_token');
+  if (push_token != null) {
+    push_notificiations_delete(push_token, {
+      success: function(result) {
+      }
+    });
   }
 }
 
@@ -62,7 +84,7 @@ function onNotificationGCM(e) {
   {
     case 'registered':
       if (e.regid.length > 0) {
-
+        push_notifications_register_device_token(e.regid);
       }
       break;
     case 'message':
@@ -79,5 +101,29 @@ function onNotificationAPN(event) {
   }
   if (event.sound) {
     //navigator.notification.vibrate(2000);
+  }
+}
+
+function push_notificiations_create(data, options) {
+  try {
+    options.method = 'POST';
+    options.path = 'push_notifications';
+    options.service = 'push_notifications';
+    options.resource = 'push_notifications';
+    options.data = JSON.stringify(data);
+    Drupal.services.call(options);
+  }
+  catch (error) {
+  }
+}
+function push_notificiations_delete(token, options) {
+  try {
+    options.method = 'DELETE';
+    options.path = 'push_notifications/' + token;
+    options.service = 'push_notifications';
+    options.resource = 'push_notifications';
+    Drupal.services.call(options);
+  }
+  catch (error) {
   }
 }
